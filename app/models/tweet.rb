72413c4e513ae5c967dd4c1fd2a3957c0f25debe
Tom Grushka <tom@grushka.com>
2013-11-09 07:17:03 -0700

    tweet_id.to_s

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,7 @@
 class Tweet < ActiveRecord::Base
   def self.pull_tweets
     Twitter.favorites("Bahai_Quote").each do |tweet|
-      unless exists?(tweet_id: tweet.id)
+      unless exists?(tweet_id: tweet.id.to_s)
         create!(
           tweet_id: tweet.id,
           content: tweet.text,
