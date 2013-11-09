38dfd3cc7f2be8cf2e02b7cc2d676da5655cbf3e
Tom Grushka <tom@grushka.com>
2013-11-09 14:54:02 -0700

    AR logger STDOUT

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module Cpnjysep
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+    #ActiveRecord::Base.connection_pool.clear_reloadable_connections!
+    
     console do
       AwesomePrint.pry!
     
