331db08a14240a8f80da81221ae27a40a92a5326
Tom Grushka <tom@grushka.com>
2013-11-09 06:52:09 -0700

    fall back to asset compiler

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Cpnjysep::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
