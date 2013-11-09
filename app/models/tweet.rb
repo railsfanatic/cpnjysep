cc3e523adbc970b316833770c852aeb305f09ddc
Tom Grushka <tom@grushka.com>
2013-11-09 07:14:17 -0700

    since_id is invalid on Twitter gem

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ActiveRecord::Base
   def self.pull_tweets
-    Twitter.favorites("Bahai_Quote", since_id: maximum(:tweet_id)).each do |tweet|
+    Twitter.favorites("Bahai_Quote").each do |tweet|
       unless exists?(tweet_id: tweet.id)
         create!(
           tweet_id: tweet.id,
